029631281ba6ea8bd25916f2d9832e8d29d0783a
Stoycho Petrov <scyberboy@bateslack.stp.bg>
2017-01-31 07:48:02 +0200

    initial ... :)

---
+++ stp_Adv_C_threads_Pi-Leibniz.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <math.h>
+#include <unistd.h>
+
+#define NUM_THREADS 3
+
+const double precision = 1e-12;
+double fPi = 0.0;
+
+
+
+typedef struct thread_data_t
+{
+   pthread_t th_id;
+	int th_num;
+	float sign;
+	int divisor;
+	double result;
+} thread_data;
+
+
+void * worker_func( void * t_data )
+{
+   thread_data *w_res_ptr = (thread_data *) t_data;
+   int l_divisor = w_res_ptr->divisor;
+   float l_sign = w_res_ptr->sign;
+   
+   while( 1.0 / l_divisor > precision )
+   {
+      w_res_ptr->result += ( ( l_sign * 1.0 ) / l_divisor );
+      
+      l_divisor += 2*NUM_THREADS; // -3, +5, -7, +9, -11, +13, etc...
+      l_sign = ( NUM_THREADS % 2 == 0) ? l_sign : -1.0 * l_sign;            
+   }
+
+
+   // we have to return a pointer to the thread's data, otherwise the join won't get correct stuff !
+   return t_data; //w_res_ptr;
+}
+
+
+int main (int argc, char* argv[])
+{
+	int i = 0;
+	thread_data thr_data[NUM_THREADS] = {{0}};
+	void *th_result = NULL;
+
+	// spawn the threads
+	for( i = 0 ; i < NUM_THREADS ; i++ )
+	{
+	   thr_data[i].sign = ( i % 2 == 0 ) ? -1 : 1;//pow( -1, i+1);
+	   thr_data[i].divisor = 3 + 2*i;
+	   thr_data[i].th_num = i+1;
+	   thr_data[i].result = 0.0;
+	   
+	   pthread_create( &thr_data[i].th_id, NULL, worker_func, (void *)&thr_data[i] );
+	}
+
+	// with for threads to do the work
+	sleep(1);
+
+	// calc Pi
+	fPi = 1.0;
+
+	// loop thru thread's data and add each result (join all threads)
+	for( i = 0 ; i < NUM_THREADS ; i++ )
+	{
+	   pthread_join( thr_data[i].th_id, &th_result );
+	   thread_data *l_res_ptr = (thread_data *) th_result;
+	   
+	   fPi += l_res_ptr->result;
+
+	   printf( "Worker %d(%p) yields %.10g ==> 1/4th of fPi(%.10g)\n", l_res_ptr->th_num, l_res_ptr, l_res_ptr->result, fPi );
+	   
+	}
+
+	printf( "finish: fPi(%.10g)\n", 4.0 * fPi );
+
+	return 0;
+}
